ac97500588a249716bd859c4595e450a39f2fbf1
ndd305 <ndd3591@gmail.com>
2015-03-23 21:17:46 +0700

    ver

--- BanaSharp/Properties/AssemblyInfo.cs
+++ BanaSharp/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("5.3.23.848")]
-[assembly: AssemblyFileVersion("5.3.23.848")]
+[assembly: AssemblyVersion("5.3.23.917")]
+[assembly: AssemblyFileVersion("5.3.23.917")]
